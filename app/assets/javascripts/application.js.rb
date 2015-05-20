ceb4b6367c1d8b11c631a15aa4c7b4fea3ea46c7
Brady Wied <brady@wied.us>
2015-05-20 12:34:12 -0600

    Explain

--- app/assets/javascripts/application.js.rb
+++ app/assets/javascripts/application.js.rb
@@ -1,4 +1,6 @@
 require 'opal'
 require 'opal_ujs'
+# Handles the opalrb->JS part, but does NOT run the code
 require_tree '.'
+# Contains stuff that actually runs the code
 require 'opal_application'
