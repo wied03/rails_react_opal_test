e9264ea5a3cfed221dba0419e993de98dec73ef0
Brady Wied <brady@wied.us>
2015-05-20 13:41:16 -0600

    Root cause

--- app/assets/javascripts/opal_application.js.rb
+++ app/assets/javascripts/opal_application.js.rb
@@ -1,4 +1,3 @@
-# https://github.com/opal/opal-rails/issues/49
 # There is a difference between sprockets (ensures the code is compiled) and making sure the browser runs it
-# this ensures the browser runs it
-Opal.require 'foobars_view'
+# require_tree does not run the code (seems to be only used for sprockets)
+require 'foobars_view'
