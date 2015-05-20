3672336921f4e5b888d0b5978d1c74b0b4680493
Brady Wied <brady@wied.us>
2015-05-20 12:48:27 -0600

    Keep following tutorial

--- app/assets/javascripts/foobars_view.js.rb
+++ app/assets/javascripts/foobars_view.js.rb
@@ -1,12 +1,20 @@
 require 'react'
 
 class HelloMessage
+  def initialize(props)
+    puts props
+  end
+
+  def component_will_mount
+    puts "will mount!"
+  end
+
   def render
-    React.create_element('div') { 'Hello world' }
+    React.create_element("div") { "Hello #{self.props[:name]}!" }
   end
 end
 
 
 Document.ready? do
-  React.render(React.create_element(HelloMessage), `document.body`)
+  React.render(React.create_element(HelloMessage, name: 'John'), `document.body`)
 end
