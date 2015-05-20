e42abd7fa57ef137ad29e8cd1ff3da2f43b7e97c
Brady Wied <brady@wied.us>
2015-05-20 14:48:28 -0600

    Real test

--- spec-opal/test_spec.js.rb
+++ spec-opal/test_spec.js.rb
@@ -3,6 +3,9 @@ require 'foobars_view'
 
 describe HelloMessage do
   it 'works' do
-    true.should == false
+    
+    result = React.render_to_static_markup(React.create_element(App))
+    
+    result.should == '<div><span>Default greeting: Cool! John!</span></div>'
   end
 end
